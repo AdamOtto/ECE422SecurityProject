9b0707c2dc2091416a17fd56d8638367d154639d
yidingfan <yiding@ualberta.ca>
2018-02-19 15:57:10 -0700

    hdf5 filebase directory

--- storage_h5.py
+++ storage_h5.py
@@ -11,9 +11,14 @@ def open_private():
 def open_public():
 	f=h5py.File(public_file,'a')
 	return f
-def write_h5(f,uid,fid,context):
-	data=np.array(f["data"])
+def write_h5(f,uid,fid,des,context):
+	if des in f:
+		grp=f[des]
+	else:
+		return None
+	data=np.array(grp["data"])
 	userdata=data[data['uid']==uid]
+	
 	newdata=np.array([(uid,fid,context)],dtype=field_type)
 	if (len(userdata)==0):
 		data=np.append(data,newdata,axis=0)
@@ -23,19 +28,42 @@ def write_h5(f,uid,fid,context):
 			data[index]=newdata
 		else:
 			data=np.append(data,newdata,axis=0)
-	f["data"].resize(data.shape)
-	f["data"][...]=data
-	f.flush()
+	grp["data"].resize(data.shape)
+	grp["data"][...]=data
+	
 	
 def initial(f):
 	initialdata=np.array([(-1,-1,"the initial of hdf5")],dtype=field_type)
-	f.create_dataset("data",data=initialdata,maxshape=(None,))
+	grp=f.create_group("test")
+	grp.create_dataset("data",data=initialdata,maxshape=(None,))
 	
-def read_h5(f):
-	return np.array(f["data"])
+def read_h5(f,des):
+	if des in f:
+		grp=f[des]
+	else:
+		return None
+	return np.array(grp["data"])
+def create_directory(f,des,loc=None):
+	initialdata=np.array([(-1,-1,"the initial of hdf5")],dtype=field_type)
+	if loc:
+		grp=f[loc]
+		sub=grp.create_group(des)
+	else:
+		sub=f.create_group(des)
+	sub.create_dataset("data",data=initialdata,maxshape=(None,))
 if __name__ == '__main__':
 	f=open_private()
-	write_h5(f,-2,-2,u"write test3")
-	test=read_h5(f)
+	test=read_h5(f,"test")
+	print("test:\n")
 	print(test)
+	print("\n")
+	test=read_h5(f,"test1")
+	print("test1:\n")
+	print(test)
+	print("\n")
+	test=read_h5(f,"uid/test/test1")
+	print("test/test1:\n")
+	print(test)
+	print("\n")
+	
 			
