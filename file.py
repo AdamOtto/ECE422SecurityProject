6951fcc1662f9f86e71bb53ad05a6d54247af823
yidingfan <yiding@ualberta.ca>
2018-03-06 14:17:47 -0700

    crypto test

--- file.py
+++ file.py
@@ -1,32 +1,50 @@
 import json
+from Crypto.Hash import SHA256
+from Crypto.Cipher import ARC2
 def file_json(user,team,filename):
 	try:
 		f=open(filename)
+		
 		fname=filename
-		data=cry_fdata(f)
+		data=cry_fdata(f,team)
 		
 		to_json={'user':user,'team':team,'fname':fname,'data':data}
-		return json.dumps(to_json)
+		return to_json
 	except:
 		return None
 
-def cry_fdata(f):
-	return f.read()
-def dcry_fname(f):
-	return f
-def dcry_fdata(f):
-	return f
+def cry_fdata(f,team):
+	
+	h=SHA256.new()
+	h.update(team)
+	c=ARC2.new(h.hexdigest(),ARC2.MODE_ECB,"")
+	data=f.read()
+	
+	if (len(data)%8==0):
+		to_crypto=data
+	else:
+		remain=8-len(data)%8
+		to_crypto=data+" "*remain
+	
+	
+		
+	return c.encrypt(to_crypto)
+
+def dcry_fdata(f,team):
+	h=SHA256.new()
+	h.update(team)
+	c=ARC2.new(h.hexdigest(),ARC2.MODE_ECB,"")
+	return c.encrypt(f)
 def loads(js):
-	d=json.loads(js)
-	fname=dcry_fname(d['fname'])
-	data=dcry_fdata(d['data'])
+	d=json.loads(js.decode('b'))
+	fname=d['fname']+'1'
+	team=d['team']
+	data=dcry_fdata(d['data'],team)
 	f=open(fname,'w')
 	f.write(data)
 	
 if __name__ == '__main__':
-	test=file_json("test","123","test1.txt")
-	print(test)
-	test2=file_json("test","123","test2.txt")
-	print (test2)
+	
+	
 	
 		
